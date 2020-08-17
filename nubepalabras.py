a7e204dc5361f1ca66f0abf250071ac2e49edf14
Rendon Quijije Ivan <ivanrendon00@gmail.com>
2020-08-16 19:33:44 -0500

    Arreglo en nubepalabras para que salgan simbolos

--- nubepalabras.py
+++ nubepalabras.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def generar_imagen(palabras , custom_mask, background = "white"):
     c_mask = np.array(Image.open(custom_mask))
-    wc = WordCloud(background_color = background,  mask = c_mask)
+    wc = WordCloud(background_color=background, mask=c_mask, regexp=r"\S[\S']+")
     wc.generate(palabras)
     plt.imshow(wc, interpolation = "bilinear")
     plt.axis("off")
